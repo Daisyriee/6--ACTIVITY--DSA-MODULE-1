01eb58224b304b6f137c05b3569761b7d431d694
Daisyriee <syriefernando@gmail.com>
2024-09-15 20:20:38 +0800

    Initial commit

---
+++ dataConversion.js
@@ -0,0 +1,20 @@
+let price = "200";
+
+let d = parseInt(price);
+console.log("Using parseInt():", d);
+let a = Number(price);
+console.log("Using Number():", a);
+let i  = +price;
+console.log("Using + operator:", i);
+
+
+let distance = 150;
+let s = distance.toString();
+console.log("Using toString():", s);
+let y = String(distance);
+console.log("Using String():", y);
+
+
+let weight = "72.5";
+let f = parseFloat(weight);
+console.log("Using parseFloat():", f);
